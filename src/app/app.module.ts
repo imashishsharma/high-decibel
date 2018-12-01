61694c191c5b9280c12b7fcb35e2e522e1abceed
Ashish <6ashishs@gmail.com>
2018-12-01 17:19:35 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import '../assets/js/agency.min.js';
+// import '../vendor/jquery/jquery.min.js';
+// import '../vendor/jquery-easing/jquery.easing.min.js';
+// import '../assets/js/jqBootstrapValidation.js';
+// import '../assets/js/contact_me.js';
+// import '../vendor/bootstrap/js/bootstrap.bundle.min.js';
+
 
 @NgModule({
   declarations: [
